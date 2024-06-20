49d69797069324856e9fc33484c995807c68bb19
dahamkakooza <k.mahad@alustudent.com>
2024-06-20 15:01:34 -0700

    hsyud

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
         ante = chr(i)
-            if ante not in "qe":
-                        print("{:s}".format(ante), end="")
+        if ante not in "qe":
+            print("{:s}".format(ante), end="")
