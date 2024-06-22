3ce67f00e2adec454de654c2a8be4b7826f36780
dahamkakooza <k.mahad@alustudent.com>
2024-06-22 03:15:41 -0700

    gfhsy

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# secure a function that retrives an element from a list like in C
-def element_at(my_list, idx):
-    if idx < 0:
-        return "None"
-    elif idx >= len(my_list):
-        return "None"
-    else:
-        return my_list[idx]
+
+
+def search_replace(my_list, search, replace):
+    return [replace if x == search else x for x in my_list]
