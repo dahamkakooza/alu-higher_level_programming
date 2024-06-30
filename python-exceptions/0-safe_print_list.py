7eb3e5b3f32fa55c9db6b9b4ceeec9a044faf097
dahamkakooza <k.mahad@alustudent.com>
2024-06-30 07:58:14 -0700

    gfhd

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0): try: print("these are not real") except: print ("return real numbers")
+
+
+def safe_print_list(my_list=[], x=0):
+    count =
+    try:
+        while count < x:
+            print(my_list[count], end="")
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
