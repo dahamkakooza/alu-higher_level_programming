79c725358765875ad6162368d3150e394cbef2df
dahamkakooza <k.mahad@alustudent.com>
2024-06-30 08:09:52 -0700

    gghdf

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,13 +2,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    count =
+    count = 0
     try:
         for i in range(x):
-            print(my_list[count], end="")
+            print(my_list[i], end="")
             count += 1
     except IndexError:
         pass
-    finally:
-        print()
-        return count
+    print()
+    return count
