67faf1b2f9ccd23cba856a4901e3a190c39f2a50
dahamkakooza <k.mahad@alustudent.com>
2024-07-01 02:05:34 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class Square which can be used as a blueprint for creating square objects.
+"""
 
-
-class Square():
-    def my_square = Square():
-        pass
+class Square:
+        """
+    The Square class serves as a template for creating square objects. It does not have any attributes or methods.
+    """
+    pass
