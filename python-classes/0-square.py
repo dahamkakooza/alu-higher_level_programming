dcdaaa99d41d52191608ad272199f36827afbed8
dahamkakooza <k.mahad@alustudent.com>
2024-07-01 01:46:54 -0700

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+class Square():
+    pass
+def my_square = Square():
+    print (type(my_square))
---
+++ python-classes/README.md
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print ("These are my python-class projects")
