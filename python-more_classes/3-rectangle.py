a9ed265198f358b110dd4ec101807985a98ffc95
dahamkakooza <k.mahad@alustudent.com>
2024-07-07 03:03:50 -0700

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -7,7 +7,7 @@ This module defines a Rectangle class.
 class Rectangle:
     """
     Represents a rectangle.
-    
+
     Attributes:
         width (int): The width of the rectangle.
         height (int): The height of the rectangle.
