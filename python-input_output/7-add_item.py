65e03ec32d88b219f4964150e377fd1dc46b91cb
dahamkakooza <k.mahad@alustudent.com>
2024-07-19 06:37:25 -0700

    tstd

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,11 +3,9 @@
 This module defines a function that adds all arguments to a Python list
 then saves them to a file
 """
-
-
 import sys
-save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
-import_module = _import_('6-load_from_json_file')
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import_module = __import__('6-load_from_json_file')
 load_from_json_file = import_module.load_from_json_file
 
 
