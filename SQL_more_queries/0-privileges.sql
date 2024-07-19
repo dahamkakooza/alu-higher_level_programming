d14d137f0203cd65084f258a2eb8dba54251b6d3
dahamkakooza <k.mahad@alustudent.com>
2024-07-19 11:08:57 -0700

    quer

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,15 +1,3 @@
--- Check if user_0d_1 exists, if not, create and grant privileges
-SELECT IF(EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost'), 'User exists', 'User does not exist') AS 'user_0d_1 status';
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Check if user_0d_2 exists, if not, create and grant privileges
-SELECT IF(EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost'), 'User exists', 'User does not exist') AS 'user_0d_2 status';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- List all privileges for user_0d_1
+-- show all privileges of data users 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List all privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
