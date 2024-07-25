183baed9743b4835b982e5302906c7604c56396f
dahamkakooza <k.mahad@alustudent.com>
2024-07-25 11:51:10 +0000

    1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Sends a GET request to the provided URL and displays the body only if the response status code is 200
-response=$(curl -s -o /dev/null -w "%{http_code}" -L "$1")[ "$response" -eq 200 ] && curl -s -L "$1"
+#!/bin/bash 
+# display the body of a file 
+curl -sLfG "$1"
